d85796ffcf35b4dfb427e8b3d868492d12e742af
emzi <emzi@users.noreply.github.com>
2016-05-27 16:51:54 +0430

    INIT: About SSGs and Hugo

---
+++ SSG+Hugo/fa/assets/js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+$(document).on('impress:stepactivate', function (event) {
+  var target = $(event.target);
+  var body = $(document.body);
+  body.removeClass(
+    'white-bg gray-bg red-bg orange-bg green-bg purple-bg blue-bg odyssey-bg');
+  if (target.hasClass('white'))
+    body.addClass('white-bg');
+  else if (target.hasClass('gray'))
+    body.addClass('gray-bg');
+  else if (target.hasClass('red'))
+    body.addClass('red-bg');
+  else if (target.hasClass('orange'))
+    body.addClass('orange-bg');
+  else if (target.hasClass('green'))
+    body.addClass('green-bg');
+  else if (target.hasClass('purple'))
+    body.addClass('purple-bg');
+  else if (target.hasClass('blue'))
+    body.addClass('blue-bg');
+  else if (target.hasClass('odyssey'))
+    body.addClass('odyssey-bg');
+});
+
+for (var s in steps)
+  steps[s]();
+
+impress().init();
+
+})
